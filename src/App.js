16ed08b50a61371f38d0547823e28015b6d5debd
Dmitry <dmitry27zh@gmail.com>
2023-09-13 19:24:38 +0300

    Add useEffect to set isLoggedIn

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Login from './components/Login/Login'
 import Home from './components/Home/Home'
 import MainHeader from './components/MainHeader/MainHeader'
 
 function App() {
+  const storedLoginInfo = localStorage.getItem('isLoggedIn')
   const [isLoggedIn, setIsLoggedIn] = useState(false)
-
+  useEffect(() => {
+    if (storedLoginInfo === '1') {
+      setIsLoggedIn(true)
+    }
+  }, [])
   const loginHandler = (email, password) => {
+    localStorage.setItem('isLoggedIn', '1')
     setIsLoggedIn(true)
   }
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn')
     setIsLoggedIn(false)
   }
 
