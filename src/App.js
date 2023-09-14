0aa57b00b1d6453677c2e8780c056324ad3176b2
Dmitry <dmitry27zh@gmail.com>
2023-09-14 12:31:21 +0300

    Use createContext and Provider to provide isLoggedIn to Navigation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import Login from './components/Login/Login'
 import Home from './components/Home/Home'
 import MainHeader from './components/MainHeader/MainHeader'
+import AuthContext from './store/auth-context'
 
 function App() {
   const storedLoginInfo = localStorage.getItem('isLoggedIn')
@@ -24,11 +25,13 @@ function App() {
 
   return (
     <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
+      <AuthContext.Provider value={{ isLoggedIn }}>
+        <MainHeader onLogout={logoutHandler} />
+        <main>
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+          {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
+      </AuthContext.Provider>
     </React.Fragment>
   )
 }
